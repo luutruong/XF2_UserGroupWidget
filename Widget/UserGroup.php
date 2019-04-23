6643de5aaa92d1cf0375859e5d3bb1bbfda3543e
Luu Truong <truonglv@outlook.com>
2019-04-23 11:31:31 +0700

    Support more options

--- Widget/UserGroup.php
+++ Widget/UserGroup.php
@@ -7,27 +7,33 @@ use XF\Widget\AbstractWidget;
 class UserGroup extends AbstractWidget
 {
     protected $defaultOptions = [
-        'user_group_ids' => []
+        'user_group_ids' => [],
+        'limit' => 5,
+        'order' => 'username',
+        'direction' => 'desc',
+        'ttl' => 60
     ];
 
     public function render()
     {
         $userGroupIds = $this->options['user_group_ids'];
         if (empty($userGroupIds)) {
-            return null;
+            return '';
+        }
+
+        $userIds = $this->getDataFromCache();
+        if ($userIds === null) {
+            $userIds = $this->getUserIdsForCache();
+        }
+
+        if (empty($userIds)) {
+            return '';
         }
 
         $finder = $this->finder('XF:User');
-        $whereOr = [
-            ['user_group_id', $userGroupIds]
-        ];
+        $finder->where('user_id', $userIds);
 
-        foreach ($userGroupIds as $userGroupId) {
-            $whereOr[] = $finder->expression('FIND_IN_SET(' . intval($userGroupId) . ', secondary_group_ids) > 0');
-        }
-
-        $finder->whereOr($whereOr);
-        $users = $finder->fetch();
+        $users = $finder->fetch()->sortByList($userIds);
 
         return $this->renderer('ugw_widget_users', [
             'users' => $users,
@@ -38,6 +44,31 @@ class UserGroup extends AbstractWidget
     public function getOptionsTemplate()
     {
         return 'admin:ugw_widget_def_options_user_groups';
+    }
+
+    protected function getUserIdsForCache()
+    {
+        $userGroupIds = $this->options['user_group_ids'];
+
+        $finder = $this->finder('XF:User');
+        $userGroupIds = array_map('intval', $userGroupIds);
+
+        $whereOr = [
+            ['user_group_id', $userGroupIds]
+        ];
+
+        $secondaryGroupsColumn = $finder->columnSqlName('secondary_group_ids');
+        foreach ($userGroupIds as $userGroupId) {
+            $whereOr[] = $finder->expression('FIND_IN_SET(' . $userGroupId . ',' . $secondaryGroupsColumn .') > 0');
+        }
+
+        $finder->order($this->options['order'], $this->options['direction']);
+
+        $finder->whereOr($whereOr);
+        $finder->limit($this->options['limit']);
+
+        $users = $finder->fetchColumns('user_id');
+        return array_column($users, 'user_id');
     }
 
     protected function getDefaultTemplateParams($context)
@@ -53,11 +84,52 @@ class UserGroup extends AbstractWidget
         return $params;
     }
 
+    protected function getDataFromCache()
+    {
+        $cache = $this->app()->cache();
+        if ($cache === null) {
+            return null;
+        }
+
+        $value = $cache->fetch($this->getCacheId());
+        if (!$value) {
+            return null;
+        }
+
+        $decoded = json_decode($value, true);
+        return is_array($decoded) ? $decoded : null;
+    }
+
+    protected function saveCacheData(array $userIds)
+    {
+        $cache = $this->app()->cache();
+        if ($cache === null) {
+            return;
+        }
+
+        $cache->save($this->getCacheId(), json_encode($userIds), $this->options['ttl'] * 60);
+    }
+
+    protected function getCacheId()
+    {
+        $encoded = json_encode($this->options);
+
+        return md5(strval($encoded));
+    }
+
     public function verifyOptions(\XF\Http\Request $request, array &$options, &$error = null)
     {
-        $options = $request->filter([
-            'user_group_ids' => 'array-uint'
-        ]);
+        $options = array_replace($options, $request->filter([
+            'user_group_ids' => 'array-uint',
+            'order' => 'str',
+            'direction' => 'str',
+            'limit' => 'uint',
+            'ttl' => 'uint'
+        ]));
+
+        if (empty($options['limit'])) {
+            $options['limit'] = 1;
+        }
 
         return true;
     }
