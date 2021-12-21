904f486eb527bf78b2af5dfaa86dcba57dccd63b
Luu Truong <truonglv@outlook.com>
2021-12-21 09:08:30 +0700

    Improvement

--- Widget/UserGroup.php
+++ Widget/UserGroup.php
@@ -2,76 +2,112 @@
 
 namespace Truonglv\UserGroupWidget\Widget;
 
+use XF\Finder\User;
 use XF\Widget\AbstractWidget;
 
 class UserGroup extends AbstractWidget
 {
+    /**
+     * @var array
+     */
     protected $defaultOptions = [
         'user_group_ids' => [],
         'limit' => 5,
         'order' => 'username',
         'direction' => 'desc',
-        'ttl' => 60
+        'cache_ttl' => 300,
+        'last_activity_start' => '',
+        'last_activity_end' => '',
+        'display_avatar' => true,
     ];
 
+    /**
+     * @return string
+     */
     public function render()
     {
-        $userGroupIds = $this->options['user_group_ids'];
-        if (empty($userGroupIds)) {
-            return '';
-        }
-
         $userIds = $this->getDataFromCache();
         if ($userIds === null) {
             $userIds = $this->getUserIdsForCache();
+            $this->saveCacheData($userIds);
         }
 
-        if (empty($userIds)) {
+        if (count($userIds) === 0) {
             return '';
         }
 
+        /** @var User $finder */
         $finder = $this->finder('XF:User');
         $finder->where('user_id', $userIds);
+        $finder->isValidUser();
 
         $users = $finder->fetch()->sortByList($userIds);
+        if ($users->count() > $this->options['limit']) {
+            $users = $users->slice(0, $this->options['limit']);
+        }
 
         return $this->renderer('ugw_widget_users', [
             'users' => $users,
-            'title' => $this->getTitle()
+            'title' => $this->getTitle(),
+            'options' => $this->options
         ]);
     }
 
+    /**
+     * @return string
+     */
     public function getOptionsTemplate()
     {
         return 'admin:ugw_widget_def_options_user_groups';
     }
 
-    protected function getUserIdsForCache()
+    protected function getUserIdsForCache(): array
     {
-        $userGroupIds = $this->options['user_group_ids'];
-
+        $options = $this->options;
         $finder = $this->finder('XF:User');
+
+        $userGroupIds = $options['user_group_ids'];
         $userGroupIds = array_map('intval', $userGroupIds);
+        if (count($userGroupIds) > 0) {
+            $whereOr = [
+                ['user_group_id', $userGroupIds]
+            ];
 
-        $whereOr = [
-            ['user_group_id', $userGroupIds]
-        ];
+            $secondaryGroupsColumn = $finder->columnSqlName('secondary_group_ids');
+            foreach ($userGroupIds as $userGroupId) {
+                $whereOr[] = $finder->expression('FIND_IN_SET(' . $userGroupId . ',' . $secondaryGroupsColumn . ') > 0');
+            }
 
-        $secondaryGroupsColumn = $finder->columnSqlName('secondary_group_ids');
-        foreach ($userGroupIds as $userGroupId) {
-            $whereOr[] = $finder->expression('FIND_IN_SET(' . $userGroupId . ',' . $secondaryGroupsColumn . ') > 0');
+            $finder->whereOr($whereOr);
         }
 
-        $finder->order($this->options['order'], $this->options['direction']);
+        if ($options['last_activity_start'] !== '') {
+            $dt = \DateTime::createFromFormat('Y-m-d', $options['last_activity_start']);
+            if ($dt !== false) {
+                $dt->setTime(0, 0, 0);
+                $finder->where('last_activity', '>=', $dt->getTimestamp());
+            }
+        }
+        if ($options['last_activity_end'] !== '') {
+            $dt = \DateTime::createFromFormat('Y-m-d', $options['last_activity_end']);
+            if ($dt !== false) {
+                $dt->setTime(23, 59, 59);
+                $finder->where('last_activity', '<=', $dt->getTimestamp());
+            }
+        }
 
-        $finder->whereOr($whereOr);
-        $finder->limit($this->options['limit']);
+        $finder->order($options['order'], $options['direction']);
+        $finder->limit($options['limit'] * 2);
 
         $users = $finder->fetchColumns('user_id');
 
         return array_column($users, 'user_id');
     }
 
+    /**
+     * @param mixed $context
+     * @return array
+     */
     protected function getDefaultTemplateParams($context)
     {
         $params = parent::getDefaultTemplateParams($context);
@@ -85,7 +121,7 @@ class UserGroup extends AbstractWidget
         return $params;
     }
 
-    protected function getDataFromCache()
+    protected function getDataFromCache(): ?array
     {
         $cache = $this->app()->cache();
         if ($cache === null) {
@@ -93,44 +129,69 @@ class UserGroup extends AbstractWidget
         }
 
         $value = $cache->fetch($this->getCacheId());
-        if (!$value) {
+        if (!is_array($value)) {
             return null;
         }
 
-        $decoded = json_decode($value, true);
-
-        return is_array($decoded) ? $decoded : null;
+        return $value;
     }
 
-    protected function saveCacheData(array $userIds)
+    protected function saveCacheData(array $userIds): void
     {
         $cache = $this->app()->cache();
-        if ($cache === null) {
+        if ($cache === null
+            || $this->options['cache_ttl'] <= 0
+        ) {
             return;
         }
 
-        $cache->save($this->getCacheId(), json_encode($userIds), $this->options['ttl'] * 60);
+        $cache->save($this->getCacheId(), $userIds, $this->options['cache_ttl']);
     }
 
-    protected function getCacheId()
+    protected function getCacheId(): string
     {
-        $encoded = json_encode($this->options);
-
-        return md5(strval($encoded));
+        return md5(
+            $this->widgetConfig->widgetKey
+            . $this->widgetConfig->widgetId
+            . serialize($this->options)
+        );
     }
 
+    /**
+     * @param \XF\Http\Request $request
+     * @param array $options
+     * @param mixed $error
+     * @return bool
+     */
     public function verifyOptions(\XF\Http\Request $request, array &$options, &$error = null)
     {
-        $options = array_replace($options, $request->filter([
+        $options = $request->filter([
             'user_group_ids' => 'array-uint',
             'order' => 'str',
             'direction' => 'str',
             'limit' => 'uint',
-            'ttl' => 'uint'
-        ]));
+            'cache_ttl' => 'uint',
+            'last_activity_start' => 'datetime',
+            'last_activity_end' => 'datetime',
+            'display_avatar' => 'bool'
+        ]);
 
-        if (empty($options['limit'])) {
+        if ($options['limit'] < 1) {
             $options['limit'] = 1;
+        }
+
+        $visitor = \XF::visitor();
+        $language = \XF::app()->language($visitor->language_id);
+        if ($options['last_activity_start'] > 0) {
+            $options['last_activity_start'] = $language->date($options['last_activity_start'], 'picker');
+        } else {
+            $options['last_activity_start'] = '';
+        }
+
+        if ($options['last_activity_end'] > 0) {
+            $options['last_activity_end'] = $language->date($options['last_activity_end'], 'picker');
+        } else {
+            $options['last_activity_end'] = '';
         }
 
         return true;
